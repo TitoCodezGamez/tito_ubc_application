4a0791f6b5f5c194e999f0a5bb79f367b8ee3c32
Taha Said <tahasaid2008@gmail.com>
2025-11-24 16:53:43 -0800

    Small css improvement

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'.    conn = self.create_connection(alias)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
